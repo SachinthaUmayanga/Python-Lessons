baff69d5249fcd644096b34f713ebf5bdb028956
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2024-04-21 11:41:18 +0530

    tring for datatypes

---
+++ First Week/data_types.py
@@ -0,0 +1,29 @@
+x="Python{}"
+y="\nHello world---"
+z="""\nHEllo world..."""
+
+#check the data type
+#print(type(x),x)
+
+# print(x,y,z)
+
+#print the first letter of the x
+# print(x[0])
+
+#print the second letter of the last side
+# print(x[-2])
+
+a=0
+# print(type(a))
+
+#to pass the value use the {} and print code like the following
+# print(x.format(a))
+
+# 
+# print (x+y+str(a))
+
+
+# print(f"Sachintha {x}")
+
+b=0.0
+print(type(b))
